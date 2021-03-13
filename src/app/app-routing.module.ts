3207f612b75edaa1b9ea271fb2ec4f248b4d634d
Shehan Lakshmen <ktsravindu@gmail.com>
2021-03-13 12:05:28 +0530

    Commit loging component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BettingMainPageWithHeaderFooterComponent } from './View/betting-main-page-with-header-footer/betting-main-page-with-header-footer.component';
+import { LoginComponent } from './View/login/login.component';
 
 const routes: Routes = [
   { path: '', component: BettingMainPageWithHeaderFooterComponent, pathMatch: 'full' },
-  {
-    path: 'betting-main-page-with-header-footer',
-    component: BettingMainPageWithHeaderFooterComponent,
-  }
+  { path: 'login', component: LoginComponent },
+  { path: 'betting-main-page-with-header-footer', component: BettingMainPageWithHeaderFooterComponent }
+
+  
 ];
 
 @NgModule({
